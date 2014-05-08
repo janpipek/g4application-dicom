14622cd454e4e5ef2f53de4d99004d3f96c15a3b
Jan Pipek <jan.pipek@gmail.com>
2014-05-08 14:11:22 +0200

    Prepare material database for nested parameterisation

--- include/DicomMaterialDatabase.hh
+++ include/DicomMaterialDatabase.hh
@@ -18,16 +18,42 @@ namespace g4dicom
     class VDicomMaterialDatabase
     {
     public:
+        /**
+         * @brief Material for a voxel.
+         */
         virtual G4Material* GetMaterial(DicomData* data, int x, int y, int z) = 0;
+
+        /**
+         * @brief Material for empty voxels.
+         */
+        virtual G4Material* GetDefaultMaterial();
+
+        /**
+         * @brief All materials created by the database.
+         *
+         * Every possible value returned for the voxels
+         * has to be included. This vector is used by
+         * the nested parameterisation.
+         */
+        virtual std::vector<G4Material*> GetAllMaterials() = 0;
     };
 
     class DicomMaterialDatabase : public VDicomMaterialDatabase
     {
     public:
+        DicomMaterialDatabase();
+
         virtual G4Material* GetMaterial(DicomData* data, int x, int y, int z);
 
+        /**
+         * @brief HU value, under which the material is considered to be air.
+         */
+        const int minHU = -900;
+
+        virtual std::vector<G4Material*> GetAllMaterials();
+
     protected:
-        virtual G4Material* GetMaterialHU(int hu);
+        virtual G4Material* GetMaterialByHU(int hu);
 
     private:
         std::map<int, G4Material*> _materials;
--- src/DicomMaterialDatabase.cc
+++ src/DicomMaterialDatabase.cc
@@ -4,9 +4,19 @@
 #include <G4NistManager.hh>
 
 using namespace g4dicom;
+using namespace std;
 
-G4Material *DicomMaterialDatabase::GetMaterialHU(int hu)
+G4Material *VDicomMaterialDatabase::GetDefaultMaterial()
 {
+    return G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR");
+}
+
+G4Material* DicomMaterialDatabase::GetMaterialByHU(int hu)
+{
+    if (hu < minHU)
+    {
+        return GetDefaultMaterial();
+    }
     if (_materials.find(hu) == _materials.end())
     {
         G4NistManager* nistManager = G4NistManager::Instance();
@@ -16,11 +26,25 @@ G4Material *DicomMaterialDatabase::GetMaterialHU(int hu)
     return _materials[hu];
 }
 
-G4Material* DicomMaterialDatabase::GetMaterial(DicomData* data, int x, int y, int z)
+DicomMaterialDatabase::DicomMaterialDatabase()
 {
-    return GetMaterialHU(data->GetValue(x, y, z));
+    _materials[minHU] = GetDefaultMaterial();
 }
 
+G4Material* DicomMaterialDatabase::GetMaterial(DicomData* data, int x, int y, int z)
+{
+    return GetMaterialByHU(data->GetValue(x, y, z));
+}
+
+vector<G4Material*> DicomMaterialDatabase::GetAllMaterials()
+{
+    vector<G4Material*> materials;
+    for (auto it = _materials.begin(); it != _materials.end(); it++)
+    {
+        materials.push_back(it->second);
+    }
+    return materials;
+}
 
 double primitiveHUtoDensity(double hu)
 {
