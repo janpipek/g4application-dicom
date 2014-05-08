2dfe6e96065e2b475ca4caaf2230a53db90e49a0
Jan Pipek <jan.pipek@gmail.com>
2014-05-08 15:22:54 +0200

    Introduce VoxelParameterisation

---
+++ include/VoxelParameterisation.hh
@@ -0,0 +1,44 @@
+#ifndef VOXELPARAMETERISATION_HH
+#define VOXELPARAMETERISATION_HH
+
+#include <G4VNestedParameterisation.hh>
+#include <vector>
+#include <boost/multi_array.hpp>
+
+namespace g4dicom
+{
+    class VDicomMaterialDatabase;
+    class DicomData;
+
+    class VoxelParameterisation : public G4VNestedParameterisation
+    {
+    // G4VNestedParameterisation interface
+    public:
+        virtual G4Material *ComputeMaterial(G4VPhysicalVolume* currentVol,
+                                            const G4int repNo, const G4VTouchable* parentTouch);
+
+        virtual void ComputeTransformation(const G4int, G4VPhysicalVolume*) const;
+
+        virtual G4int GetNumberOfMaterials() const;
+
+        virtual G4Material* GetMaterial(G4int idx) const;
+
+    public:
+        VoxelParameterisation(VDicomMaterialDatabase* materialDatabase, DicomData* dicomData);
+
+    private:
+        DicomData* _dicomData;
+
+        VDicomMaterialDatabase* _materialDatabase;
+
+        boost::multi_array<G4Material*, 3> _voxelMaterials;
+
+        std::vector<G4Material*> _materials;
+
+        std::vector<int> _dims;
+
+        double _zHalfSize;
+    };
+}
+
+#endif // VOXELPARAMETERISATION_HH
---
+++ src/VoxelParameterisation.cc
@@ -0,0 +1,67 @@
+#include "VoxelParameterisation.hh"
+
+#include <G4VTouchable.hh>
+#include <G4VPhysicalVolume.hh>
+
+#include "DicomMaterialDatabase.hh"
+#include "DicomData.hh"
+
+using namespace g4dicom;
+using namespace boost;
+using namespace std;
+
+G4Material* VoxelParameterisation::ComputeMaterial(G4VPhysicalVolume *currentVol, const G4int repNo, const G4VTouchable *parentTouch)
+{
+    if (!parentTouch)
+    {
+        return _materialDatabase->GetDefaultMaterial();
+    }
+    int x = parentTouch->GetReplicaNumber(1);
+    int y = parentTouch->GetReplicaNumber(0);
+
+    return _voxelMaterials[x][y][repNo];
+}
+
+void VoxelParameterisation::ComputeTransformation(const G4int copyNo,
+        G4VPhysicalVolume* physVol) const
+{
+    // Adapted from DicomNestedPhantomParameterisation (in the DICOM example)
+    G4ThreeVector position(0., 0., (2 * copyNo + 1 - _dims[2]) * _zHalfSize);
+    physVol->SetTranslation(position);
+}
+
+G4int VoxelParameterisation::GetNumberOfMaterials() const
+{
+    return _materials.size();
+}
+
+G4Material* VoxelParameterisation::GetMaterial(G4int idx) const
+{
+    return _materials[idx];
+}
+
+VoxelParameterisation::VoxelParameterisation(VDicomMaterialDatabase* materialDatabase, DicomData* dicomData)
+    : _materialDatabase(materialDatabase),
+      _dicomData(dicomData)
+{
+    // Prepare storage for materials
+    _dims = dicomData->GetDimensions();
+    _zHalfSize = dicomData->GetVoxelSize()[2] / 2.0;
+    _voxelMaterials.resize(boost::extents[_dims[0]][_dims[1]][_dims[2]]);
+
+    // Fill in materials
+    for (int x = 0; x < _dims[0]; x++)
+    {
+        for (int y = 0; y < _dims[1]; y++)
+        {
+            for (int z = 0; z < _dims[2]; z++)
+            {
+                _voxelMaterials[x][y][z] =
+                     _materialDatabase->GetMaterial(dicomData, x, y, z);
+            }
+        }
+    }
+
+    // Get list of materials to fulfill G4NestedParameterisation interface
+    _materials = materialDatabase->GetAllMaterials();
+}
